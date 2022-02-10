6a072424bc6bfad6e84617cebd7e30001b2d3720
HinaM <55233191+HinaM@users.noreply.github.com>
2022-02-11 01:13:45 +0800

    excel測試檔

---
+++ excel_test.py
@@ -0,0 +1,8 @@
+from fileinput import filename
+import gspread
+gc=gspread.service_account(filename='fjuim-340916-7e527f907c19.json')
+sh=gc.open_by_key('1LozvTUSglnM_TtYO1tyDROkFkpX4lrlaVD1tC0911XM')
+worksheet=sh.sheet1
+
+res=worksheet.get_all_records()
+print(res)
