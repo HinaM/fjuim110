6eb322a0a6a339fa96d7cb50d8b266b6da3fe4a9
Fjuim110 <fjuim110@gmail.com>
2022-03-07 11:17:17 +0800

    test

--- app.py
+++ app.py
@@ -365,7 +365,8 @@ def handle_message(event):
                         ]
                     )
         '''
-        carousel_template_message1 = TemplateSendMessage(
+        
+        carousel_template_message = TemplateSendMessage(
             alt_text='遊戲地圖',
             template=CarouselTemplate(
                 columns=[
@@ -406,53 +407,6 @@ def handle_message(event):
                             MessageAction(
                                 label='建築介紹',
                                 text='中美堂介紹'
-                            )
-                        ]
-                    )
-                ]
-            )
-        )
-        carousel_template_message = TemplateSendMessage(
-            alt_text='Carousel template',
-            template=CarouselTemplate(
-                columns=[
-                    CarouselColumn(
-                        thumbnail_image_url='https://example.com/item1.jpg',
-                        title='this is menu1',
-                        text='description1',
-                        actions=[
-                            PostbackAction(
-                                label='postback1',
-                                display_text='postback text1',
-                                data='action=buy&itemid=1'
-                            ),
-                            MessageAction(
-                                label='message1',
-                                text='message text1'
-                            ),
-                            URIAction(
-                                label='uri1',
-                                uri='http://example.com/1'
-                            )
-                        ]
-                    ),
-                    CarouselColumn(
-                        thumbnail_image_url='https://example.com/item2.jpg',
-                        title='this is menu2',
-                        text='description2',
-                        actions=[
-                            PostbackAction(
-                                label='postback2',
-                                display_text='postback text2',
-                                data='action=buy&itemid=2'
-                            ),
-                            MessageAction(
-                                label='message2',
-                                text='message text2'
-                            ),
-                            URIAction(
-                                label='uri2',
-                                uri='http://example.com/2'
                             )
                         ]
                     )
